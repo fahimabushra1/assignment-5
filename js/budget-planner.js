9fe19fe4f2556b528fabbe9d2467103238392312
fahimabushra1 <bushra.rifeen@gmail.com>
2022-02-18 05:59:25 +0600

    expenses

--- js/budget-planner.js
+++ js/budget-planner.js
@@ -1,1 +1,18 @@
-console.log(paisi)
+function updateTotalField() {
+
+    const foodInput = document.getElementById('food-input').value;
+    const rentInput = document.getElementById('rent-input').value;
+    const clothInput = document.getElementById('cloth-input').value;
+
+    const totalExpenses = parseFloat(foodInput) + parseFloat(rentInput) + parseFloat(clothInput);
+
+
+    const total = document.getElementById('total-expenses');
+    total.innerText = totalExpenses;
+
+}
+
+
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    updateTotalField();
+});
